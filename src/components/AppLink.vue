86a1015a504ca909237dba2c14aa7f982d3d049e
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-06 10:57:46 +0100

    created: a reuseable component call AppLink, rather than using RouterLink, i decided to use AppLink to make it easier

---
+++ src/components/AppLink.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+
+interface LinkProps {
+  to: string
+}
+
+defineProps<LinkProps>()
+</script>
+
+<template>
+  <RouterLink :to="to">
+    <slot></slot>
+  </RouterLink>
+</template>
+
+<style scoped></style>
