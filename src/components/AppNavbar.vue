14fac800943662d9678ab379854becbfe8dd7e8b
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-06 13:27:47 +0100

    added: hover effect for navbar links

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -14,15 +14,42 @@ import JesutofunmiPng from '@/assets/image/Jesutofunmi.jpg'
         </div>
       </Link>
       <nav class="flex items-center gap-10">
-        <ul class="font-switzer-md text-xl flex items-center space-x-20">
+        <ul class="font-switzer-md text-xl flex items-center space-x-20 mt-1">
+          <li></li>
           <li>
-            <Link to="/">Home</Link>
+            <Link class="inline-block relative group" to="/">
+              <span
+                class="group-hover:text-[#8631F4] transition-all duration-500 ease-in-out inline-block pb-1"
+                >Home</span
+              >
+              <div
+                class="w-20 h-[0.1rem] bg-[#8631F4] mt-5 absolute bottom-0 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out"
+              ></div>
+            </Link>
           </li>
           <li>
-            <Link to="#project">Project</Link>
+            <Link class="inline-block relative group" to="#project">
+              <span
+                class="group-hover:text-[#8631F4] transition-all duration-500 ease-in-out inline-block pb-1"
+              >
+                Project
+              </span>
+              <div
+                class="w-20 h-[0.1rem] bg-[#8631F4] mt-5 absolute bottom-0 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out"
+              ></div>
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link class="inline-block relative group" to="/about">
+              <span
+                class="group-hover:text-[#8631F4] transition-all duration-500 ease-in-out inline-block pb-1"
+              >
+                About
+              </span>
+              <div
+                class="w-20 h-[0.1rem] bg-[#8631F4] mt-5 absolute bottom-0 left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out"
+              ></div>
+            </Link>
           </li>
         </ul>
         <div>
