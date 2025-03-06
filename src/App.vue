eedb555a8b19e99b383771061b9ae35c2d55afdc
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-06 10:26:48 +0100

    This is the App vue file

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+import AppNavbar from '@/components/AppNavbar.vue'
+import AppFooter from '@/components/AppFooter.vue'
+</script>
+
+<template>
+  <AppNavbar />
+  <RouterView />
+  <AppFooter />
+</template>
+
+<style scoped></style>
