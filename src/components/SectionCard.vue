35240c0875e864a76ff44555e1008435b1e15c81
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-14 17:47:52 +0100

    created a component, this componets (SectionCard) helps not to allow the text to touch the edges of the screen

---
+++ src/components/SectionCard.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps({
+  defaultClass: {
+    type: String,
+    default: 'max-w-7xl mx-auto px-5 lg:px-8',
+  },
+})
+</script>
+
+<template>
+  <section :class="['class', defaultClass]">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped></style>
