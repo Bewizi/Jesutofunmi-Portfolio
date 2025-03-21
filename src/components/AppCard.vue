f9a585fc97851292d2413f3f8cc56b3ba4e3a25d
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-21 12:54:57 +0100

    Created a component called AppCard this is uses in  a section called screen designed by jesutofunmi

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,11 +1,42 @@
-<script setup lang="ts">
-
+<script lang="ts" setup>
+defineProps<{
+  defaultClass?: string
+  mainHeader?: string
+  mainText?: string
+  subText?: string
+  image?: string
+  imageAlt?: string
+}>()
 </script>
 
 <template>
-  $END$
+  <section :class="['flex justify-between items-center gap-[80px]', defaultClass]">
+    <!--    image-->
+    <figure v-if="image">
+      <img :alt="imageAlt" :src="image" />
+    </figure>
+    <!--    image-->
+
+    <!--      text-->
+    <div>
+      <h1 v-if="mainHeader" class="text-[#0C0C0C] font-switzer-md text-3xl mb-5">
+        {{ mainHeader }}
+      </h1>
+      <p
+        v-if="mainText"
+        class="text-[#484848] text-lg font-switzer-regular leading-normal max-w-4xl mb-8"
+      >
+        {{ mainText }}
+      </p>
+      <p
+        v-if="subText"
+        class="text-[#484848] text-lg font-switzer-regular leading-normal max-w-4xl"
+      >
+        {{ subText }}
+      </p>
+    </div>
+    <!--      text-->
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
