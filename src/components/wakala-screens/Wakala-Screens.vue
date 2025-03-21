3a7d62630ee7874cc071875e4baa36c01710e87e
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-21 13:01:49 +0100

    Created a file Wakala-Screens, and imported in my wakalaView so as not to make WakalaView a lot of code

---
+++ src/components/wakala-screens/Wakala-Screens.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import SectionCard from '@/components/SectionCard.vue'
+import AppCard from '@/components/AppCard.vue'
+</script>
+
+<template>
+  <SectionCard class="mt-32">
+    <AppCard
+      default-class="mb-20"
+      image="/image/Wakala/Onboarding-Screen.png"
+      image-alt="Onboarding Screen"
+      main-header="Onboarding Screen"
+      main-text="The onboarding screen for WakalaPay was designed with clarity and ease in mind. From the moment users launch the app, they’re guided through a smooth, intuitive process that highlights key features—seamless crypto-to-cash conversion, low fees, and secure transactions. The design focuses on simplicity, using clean layouts, engaging visuals, and concise messaging to ensure users quickly understand the value of WakalaPay."
+    />
+
+    <AppCard
+      default-class="flex-row-reverse mb-20"
+      image="/image/Wakala/Authentication-Screen.png"
+      image-alt="Authentication Screen"
+      main-header="Authentication Screen"
+      main-text="The authentication screen for WakalaPay is designed to be secure, intuitive, and user-friendly. With a minimal yet functional layout, users can effortlessly log in or sign up using their email, phone number, or biometric authentication for a faster experience. The design ensures clarity with clear input fields, error handling, and a seamless flow that eliminates friction."
+    />
+
+    <AppCard
+      default-class="mb-20"
+      image="/image/Wakala/Wallet-Connection.png"
+      image-alt="Wallet Connection"
+      main-header="Wallet Connection"
+      main-text="The wallet connection screens for WakalaPay are designed for seamless and secure access, ensuring users can quickly link their crypto wallets with minimal effort. The interface supports multiple connection methods, including QR scanning, wallet extensions, and manual key input, giving users flexibility"
+    />
+
+    <AppCard
+      default-class="flex-row-reverse mb-20"
+      image="/image/Wakala/Home-Screen.png"
+      image-alt="Home Screen"
+      main-header="HomeScreen"
+      main-text="To enhance user engagement, the homepage also includes a referral section, guiding users on how to invite friends and earn rewards. Every element is structured to provide a seamless experience, ensuring users can navigate effortlessly while maintaining full control over their funds"
+    />
+
+    <AppCard
+      default-class="mb-20"
+      image="/image/Wakala/Bank-Details.png"
+      image-alt="Add Bank Details Screen"
+      main-header="Add Bank Details"
+      main-text="The Bank Details screen in WakalaPay ensures a smooth and secure way for users to link their local bank accounts. 🔗💳"
+      sub-text="Since seamless cashing out is essential, this screen is designed with simplicity in mind—users can add their bank name, account number, and verify ownership effortlessly. With clear feedback and security measures in place, WakalaPay ensures that withdrawals are fast and hassle-free."
+    />
+  </SectionCard>
+</template>
+
+<style scoped></style>
