f6ffa4c8c67e6235711a793ffcbc6e6eb49d3e16
Bewizi <toluwalase.bewaji@yahoo.com>
2025-03-22 13:40:34 +0100

    Created a notfound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,20 @@
-<script setup lang="ts">
-
+<script lang="ts" setup>
+import SectionCard from '@/components/SectionCard.vue'
+import Link from '@/components/AppLink.vue'
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  $END$
+  <SectionCard>
+    <div class="flex flex-col items-center justify-center space-y-4">
+      <h1 class="text-4xl font-bold text-center">404</h1>
+      <p class="text-lg text-center">Oops! The page you are looking for does not exist.</p>
+
+      <Link class="text-center flex items-center gap-2" to="/">
+        <Icon icon="carbon:arrow-left" /> Go back to home</Link
+      >
+    </div>
+  </SectionCard>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
